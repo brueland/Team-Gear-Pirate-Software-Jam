9fbaf660b11cbbd960a9e2cf049c5763575a3b4c
brueland <wesbrueland@gmail.com>
2024-07-19 18:38:56 -0500

    modified Ghost to follow, added new lantern asset pinned to character as psuedo light source for now

--- Scripts/Enemies/Ghost/Ghost.gd
+++ Scripts/Enemies/Ghost/Ghost.gd
@@ -3,47 +3,54 @@ class_name Ghost
 
 var player: CharacterBody2D
 
-@export var speed: float = 10.0
-@export var dir:Vector2
-
+@export var speed: float = 50.0
 @export var is_Chasing: bool
-var is_near_light: bool
-@onready var facing_Right: bool = true
+
+var direction: int = 1
+var sprite_direction: String = "right_"
 
 @onready var staticsmokeParticles = $GhostSprite/StaticSmokeParticles
 @onready var trailParticles = $GhostSprite/TrailParticles
+@onready var sprite = $GhostSprite
+
+var is_near_light: bool
+
 
 func _ready():
+	sprite.play("Idle")
 	player = GlobalReferences.playerBody
 
-func  _process(delta):
-	move(delta)
+func  _physics_process(delta):
+	if is_Chasing:
+		chase_player(delta)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, delta)
 	handle_animation()
 	handle_particle_position()
-	#print(facing_Right)
-
-func move(delta):
-	if!is_Chasing:
-		pass
-	else:
-		velocity = position.direction_to(player.position) * speed
-	dir.x = abs(velocity.x) / velocity.x
 	move_and_slide()
+
+func chase_player(delta):
+	velocity += position.direction_to(player.position) * speed * delta
 	
-	
-func handle_animation():
-	var animated_sprite = $GhostSprite
-	animated_sprite.play("Idle")
-	if dir.x < 0:
-		animated_sprite.flip_h = true
-	elif dir.x > 0:
-		animated_sprite.flip_h = false
-	facing_Right = !animated_sprite.flip_h
+	if velocity.x < 0.0:
+		direction = -1
+	elif velocity.x > 0.0:
+		direction = 1
+
+func handle_animation():	
+	if direction < 0:
+		sprite_direction = "right_"
+		sprite.flip_h = true
+	elif direction > 0:
+		sprite_direction = "left_"
+		sprite.flip_h = false
 
 func handle_particle_position():
-	if facing_Right:
+	if direction > 0:
 		staticsmokeParticles.position = Vector2(-11,10)
 		trailParticles.position = Vector2(-8,9)
-	elif  !facing_Right:
+	else:
 		staticsmokeParticles.position = Vector2(11,10)
 		trailParticles.position = Vector2(8,9)
+		
+# _on_area or body entered, detect lantern area and repel
