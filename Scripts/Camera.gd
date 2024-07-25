dcb0e26b4a5615fd896f39adbb6a51eb2ffdf7fc
brueland <wesbrueland@gmail.com>
2024-07-24 22:08:51 -0500

    adding camera bounding and new Camera script to auto adjust camera bounds

---
+++ Scripts/Camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+func set_camera_bounds(camera_bounds):
+	limit_bottom = camera_bounds.position.y + camera_bounds.shape.size.y/2.
+	limit_top = camera_bounds.position.y - camera_bounds.shape.size.y/2.
+	limit_left = camera_bounds.position.x - camera_bounds.shape.size.x/2.
+	limit_right = camera_bounds.position.x + camera_bounds.shape.size.x/2.
+	
+	# need to handle camera zoom, currently setting zoom to constant size in main camera
