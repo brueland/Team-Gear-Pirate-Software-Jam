b34ea7747fae602499b3eab3faeb08d64faf2acf
brueland <wesbrueland@gmail.com>
2024-07-28 02:02:22 -0500

    take that you dirty ghosts, can dissolve em with the lantern and pick up/put down the lantern. Can't grapple or mantle when holding lantern

--- Scripts/Enemies/Ghost/Ghost.gd
+++ Scripts/Enemies/Ghost/Ghost.gd
@@ -5,28 +5,49 @@ var player: CharacterBody2D
 
 @export var speed: float = 50.0
 @export var is_Chasing: bool
+@export var max_health: float = 10.0
+@onready var current_health: float = max_health
 
+@export var shader_resource: Shader
+@export var shader_noise_texture: Texture
+
+var in_light: bool = false
 var direction: int = 1
 var sprite_direction: String = "right_"
+var dying: bool = false
 
 @onready var staticsmokeParticles = $GhostSprite/StaticSmokeParticles
 @onready var trailParticles = $GhostSprite/TrailParticles
 @onready var sprite = $GhostSprite
 @onready var collider = $GhostCollider
 
-var is_near_light: bool
-
 func _ready():
 	sprite.play("Idle")
 	player = GlobalReferences.playerBody
+	var unique_material = ShaderMaterial.new()
+	unique_material.shader = shader_resource
+	sprite.material = unique_material
+	
+	sprite.material.set_shader_parameter("alpha", 1)
+	sprite.material.set_shader_parameter("distortion_strength", 0)
+	sprite.material.set_shader_parameter("dissolve_percent", 0)
+	sprite.material.set_shader_parameter("dissolve_noise", shader_noise_texture)
+	sprite.material.set_shader_parameter("dissolve_noise2", shader_noise_texture)
+	sprite.material.set_shader_parameter("dissolve_noise3", shader_noise_texture)
 
 func  _physics_process(delta):
-	if is_Chasing:
+	if is_Chasing and !in_light:
 		chase_player(delta)
 	else:
-		velocity = lerp(velocity, Vector2.ZERO, delta)
-	handle_animation()
-	handle_particle_position()
+		velocity = lerp(velocity, Vector2.ZERO, delta*40)
+		
+	if !dying:
+		handle_animation()
+		handle_particle_position()
+		handle_particle_ratio()
+		heal(delta)
+	else:
+		dying_sequence(delta)
 	move_and_slide()
 
 func chase_player(delta):
@@ -56,3 +77,30 @@ func handle_particle_position():
 		staticsmokeParticles.position = Vector2(11,10)
 		trailParticles.position = Vector2(8,9)
 		
+func handle_particle_ratio():
+	staticsmokeParticles.amount_ratio = clamp(current_health/max_health, 0.1, 1.0)
+
+func take_damage(damage_amount):
+	current_health -= damage_amount
+	current_health = max(current_health, 0)
+		
+	if current_health <= 0:
+		dying = true
+		is_Chasing = false
+
+func dying_sequence(delta):
+	set_collision_layer_value(5, false)
+	var distortion_strength = sprite.material.get_shader_parameter("distortion_strength")
+	var dissolve_percent = sprite.material.get_shader_parameter("dissolve_percent")
+
+	if distortion_strength < 1:
+		sprite.material.set_shader_parameter("distortion_strength", clamp(distortion_strength + delta, 0, 1))
+	elif dissolve_percent < 1:
+		sprite.material.set_shader_parameter("dissolve_percent", clamp(dissolve_percent + delta, 0, 1))
+	else:
+		queue_free()
+
+func heal(delta):
+	if !dying and !in_light and current_health < max_health:
+		current_health += delta
+	current_health = min(current_health, max_health)
---
+++ Scripts/Enemies/Ghost/GhostSpawner.gd
@@ -0,0 +1,46 @@
+extends Node
+
+@export var max_ghosts: int = 5
+var current_ghosts: Array
+var spawn_timer_ready: bool = true
+
+@onready var spawn_timer: Timer = $SpawnTimer
+@onready var ghostScene : PackedScene = preload(GlobalPaths.GHOST_PATH)
+@onready var player = GlobalReferences.playerBody
+
+func _ready():
+	spawn_timer.connect("timeout", _on_spawn_timer_timeout)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if current_ghosts.size() < max_ghosts and spawn_timer_ready:
+		spawn_ghost()
+		
+func spawn_ghost():
+	var ghost = ghostScene.instantiate()
+	
+	ghost.tree_exiting.connect(_on_ghost_freeing.bind(ghost))
+	add_child(ghost)
+	
+	var speed_noise = randf_range(0.5, 1.0)
+
+	ghost.global_position = player.position + _set_random_start_position()
+	ghost.speed *= speed_noise
+	
+	current_ghosts.append(ghost)
+	spawn_timer_ready = false
+	spawn_timer.start()
+	
+func _set_random_start_position():    
+	var angle = randf() * 2 * PI 
+	var distance = randf_range(100, 300) 
+	
+	var spawn_offset = Vector2(cos(angle), sin(angle)) * distance	
+	return spawn_offset
+
+func _on_ghost_freeing(ghost):
+	# Remove the scene from the array
+	current_ghosts.erase(ghost)
+
+func _on_spawn_timer_timeout():
+	spawn_timer_ready = true
